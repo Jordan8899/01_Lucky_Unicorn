2e2aa8399ed4d3a4fd27cc656b79b080d96e4e83
Jordan8899 <burmei.jorda@awatapu.school.nz>
2021-07-07 15:01:29 +1200

    def yes no checker with added question of having fun

--- 01_yes_no_checkerv2.py
+++ 01_yes_no_checkerv2.py
@@ -1,21 +1,28 @@
 
-show_instructions = ""
-while show_instructions.lower().strip() != "xxx":
-    # Ask the user if they've played before
-    show_instructions = input("Have you played this game before? ").lower().strip()
 
-    # If they say yes, output 'program continues'
-    if show_instructions == "yes" or show_instructions == "y":
-        show_instructions = "yes"
-        print("Program Continues")
+# Functions go here...
+def yes_no(question):
+    valid = False
+    while not valid:
+        response = input(question).lower().strip()
+
+        if response == "yes" or response == "y":
+            response = "yes"
 
     # If they say no, output 'display instructions'
-    elif show_instructions == "no" or show_instructions == "n":
-        show_instructions = "no"
-        print("Display Instructions")
+        elif response == "no" or response == "n":
+            response = "no"
+            return response
+        else:
+            print("Please enter either \"Yes\" or \"No\"")
+            return response
 
-    # If they don't say yes or no, output 'error please try yes or no'
-    else:
-        print("Please enter either \"Yes\" or \"No\"")
 
-    print("You chose {}".format(show_instructions))
+# Main Routine goes here...
+show_instructions = yes_no("Have you played this game before? ")
+
+print("You chose {}".format(show_instructions))
+
+print()
+having_fun = yes_no("Are you having fun? ")
+print("You said {} to having fun".format(having_fun))
