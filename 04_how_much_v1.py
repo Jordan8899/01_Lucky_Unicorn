3bb1a46128680a12c838392c543c6e6b1b306fd9
Jordan8899 <burmei.jorda@awatapu.school.nz>
2021-07-30 10:36:36 +1200

    How much infinite looping prototype code

--- 04_how_much_v1.py
+++ 04_how_much_v1.py
@@ -3,6 +3,23 @@
 
 # Main routine goes here
 
-error = "Please enter a number between 0 - 10\n"
+error = "Please enter a whole number between 0 - 10\n"
 
-valid =
+valid = False
+while not valid:
+    try:
+        # Ask the question
+        response = int(input("How much would you like to play with? "))
+
+        # If the amount is too low / too high give
+        if 0 < response <= 10:
+            print("You have asked to play with ${}".format(response))
+
+        # Output an Error
+        else:
+            print(error)
+
+    except ValueError:
+        print(error)
+
+    # Print the
